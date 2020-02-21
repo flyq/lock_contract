0ba99a0fb23f0ba45afd5af75aefb55123de59be
flyq <flyq951@gmail.com>
2020-02-21 14:26:59 +0800

    add comments

--- README.md
+++ README.md
@@ -1,5 +1,141 @@
 # lock_contract
 
+## how to compile
 ```shell
 truffle compile
+```
+
+## api
+```jsong
+======= Renting.sol:Renting =======
+Developer Documentation
+{
+  "methods" : 
+  {
+    "addPlace(uint256,uint256)" : 
+    {
+      "details" : "\u7ba1\u7406\u5458\u6ce8\u518c\u5de5\u4f4d\uff0c\u6ce8\u518c\u5de5\u4f4d\u5373\u53ef\u51fa\u79df\u65e0\u6cd5\u8f93\u5165\u5c0f\u6570\uff0c\u76f4\u63a5\u4ee5 Wei \u8ba1\uff0c\u524d\u7aef\u8f93\u5165\u8981\u670918\u4e2a\u96f6\u5de6\u53f3\uff0c\u9ebb\u70e6\u3002",
+      "params" : 
+      {
+        "_perPrice" : "\u79df\u7528\u5de5\u4f4d\u7684\u5355\u4ef7\uff0c\u6bcf\u5c0f\u65f6\u591a\u5c11\uff080.0001 ETH\uff09\uff0c\u6bd4\u5982\u6bcf\u5c0f\u65f61ETH\uff0c\u8fd9\u91cc\u9700\u8981\u8f93\u516510000\uff0c\u8fd9\u4e48\u505a\u56e0\u4e3a"
+      }
+    },
+    "getIdFromIndex(uint256)" : 
+    {
+      "return" : "element of ids;"
+    },
+    "getPlaceInfo(uint256)" : 
+    {
+      "details" : "returns the important informations of a state",
+      "params" : 
+      {
+        "_id" : "Place id"
+      },
+      "return" : "controller, rentedFrom, rentedUntil and properties of the given index beloning to the state"
+    },
+    "getPrice(uint256,uint256)" : 
+    {
+      "details" : "the total price for rentinng the Place",
+      "params" : 
+      {
+        "_id" : "the Place id"
+      },
+      "return" : "price to be payed for renting the device"
+    },
+    "getStateCount(uint8)" : 
+    {
+      "details" : "returns the current amount of statescall upgradeAllPlaceStatus() before call this function, or the amount is not accurate;",
+      "params" : 
+      {
+        "_status" : "deviceid"
+      },
+      "return" : "amount of states of a device"
+    },
+    "getTotalAmount()" : 
+    {
+      "details" : "get the total amount of Places"
+    },
+    "rent(uint256,uint256)" : 
+    {
+      "params" : 
+      {
+        "_id" : "the Place id",
+        "_secondsToRent" : "the time of rental in seconds"
+      }
+    },
+    "rmPlace(uint256)" : 
+    {
+      "details" : "\u7ba1\u7406\u5458\u79fb\u9664\u5de5\u4f4d"
+    },
+    "stopPlace(uint256)" : 
+    {
+      "details" : "\u7ba1\u7406\u5458\u6682\u505c\u5de5\u4f4d"
+    },
+    "unStopPlace(uint256)" : 
+    {
+      "details" : "\u7ba1\u7406\u5458\u542f\u52a8\u6682\u505c\u7684\u5de5\u4f4d"
+    },
+    "upgradeAllPlaceStatus2(uint256,uint256)" : 
+    {
+      "details" : "upgrade [_a, _b] status"
+    },
+    "upgradePlaceStatus(uint256)" : 
+    {
+      "details" : "\u6d41\u7a0b\u4e0a\u662f\u79df\u6237\u5230\u671f\u81ea\u52a8\u7ec8\u6b62\u5408\u540c\uff0c\u4e0d\u7528\u8c03\u7528\u5408\u7ea6\u3002\u4f46\u662f\u5408\u7ea6\u7684\u4e00\u4e9b\u72b6\u6001\u6b64\u65f6\u9700\u8981\u66f4\u65b0\uff0c\u56e0\u6b64\u4efb\u4f55\u4eba\u90fd\u53ef\u4ee5\u8c03\u7528\u3002\u5148\u7b80\u5355\u70b9\uff0c\u4e0d\u80fd\u9884\u7ea6\u51e0\u5929\u540e\u5f00\u59cb\u7684\uff0c\u6bd4\u5982\u660e\u5929\u5230\u540e\u5929\u7684\u5de5\u4f4d\u3002\u90fd\u662f\u76f4\u63a5\u4ece now \u5230\u7ed3\u675f\u65f6\u95f4\u56e0\u6b64\u4e0d\u9700\u8981\u68c0\u6d4b start"
+    },
+    "withdraw(uint256)" : 
+    {
+      "details" : "\u7ba1\u7406\u5458\u63d0\u53d6\u6536\u76ca"
+    }
+  },
+  "title" : "The rent main contract"
+}
+User Documentation
+{
+  "methods" : {}
+}
+
+======= Roles.sol:Roles =======
+Developer Documentation
+{
+  "methods" : {},
+  "title" : "Roles"
+}
+User Documentation
+{
+  "methods" : {}
+}
+
+======= Roles.sol:SuperAdminRole =======
+Developer Documentation
+{
+  "methods" : {},
+  "title" : "SuperAdminRole"
+}
+User Documentation
+{
+  "methods" : {}
+}
+
+======= Roles.sol:WhitelistedRole =======
+Developer Documentation
+{
+  "methods" : {},
+  "title" : "WhitelistedRole"
+}
+User Documentation
+{
+  "methods" : {}
+}
+
+======= SafeMath.sol:SafeMath =======
+Developer Documentation
+{
+  "methods" : {},
+  "title" : "SafeMath v0.1.9"
+}
+User Documentation
+{
+  "methods" : {}
+}
 ```
--- contracts/Renting.sol
+++ contracts/Renting.sol
@@ -66,9 +66,11 @@ contract Renting is WhitelistedRole {
     // admin
 
     /**
-     * @dev 管理员注册工位，注册工位即可出租
-     * @param _perPrice 租用工位的单价，每小时多少（0.0001 ETH），比如每小时1ETH，这里需要输入10000，这么做因为
-     * @dev 无法输入小数，直接以 Wei 计，前端输入要有18个零左右，麻烦。
+     * @title addPlace admin new a Place
+     * @param _id the new Place id, It must not be 0.
+     * @param _perPrice 租用工位的单价，每小时多少（0.0001 ETH），比如每小时1ETH，这里需要输入10000，这么做因为无法输入小数，直接以 Wei 计，前端输入要有18个零左右，麻烦。
+     * @notice Only admin can execute it.
+     * @notice after this, user can rent it.
      */
     function addPlace(uint256 _id, uint256 _perPrice) public onlySuperAdmin {
         require(_id != 0, "the Place id shouldn't be 0"); // 因为isPlaceExist根据是否是 0 来判断是否初始化
@@ -87,7 +89,10 @@ contract Renting is WhitelistedRole {
     }
 
     /**
-     * @dev 管理员移除工位
+     * @title rmPlace 管理员删除工位
+     * @param _id the id of the Place that will be deleted.
+     * @notice Only admin can execute it.
+     * @notice Only the `Free` Place can be deleted
      */
     function rmPlace(uint256 _id) public onlySuperAdmin {
         require(isPlaceExist(_id), "the Place id doesn't exist");
@@ -111,7 +116,10 @@ contract Renting is WhitelistedRole {
     }
 
     /**
-     * @dev 管理员暂停工位
+     * @title stopPlace 管理员暂停工位
+     * @param _id the id of the Place that will be stoped.
+     * @notice Only admin can execute it.
+     * @notice Only the `Free` Place can be stoped
      */
     function stopPlace(uint256 _id) public onlySuperAdmin {
         require(isPlaceExist(_id), "the Place id doesn't exist");
@@ -124,7 +132,10 @@ contract Renting is WhitelistedRole {
     }
 
     /**
-     * @dev 管理员启动暂停的工位
+     * @title unStopPlace 管理员启动已暂停的工位
+     * @param _id the id of the Place that will be unstoped.
+     * @notice Only admin can execute it.
+     * @notice Only the `Stop` Place can be upstoped
      */
     function unStopPlace(uint256 _id) public onlySuperAdmin {
         require(isPlaceExist(_id), "the Place id doesn't exist");
@@ -137,16 +148,20 @@ contract Renting is WhitelistedRole {
     }
 
     /**
-     * @dev 管理员提取收益
+     * @title withdraw 管理员提取收益
+     * @param _amount the amount of Wei will be withdraw.
+     * @notice Only admin can execute it.
      */
-    function withdraw(uint256 amount) public onlySuperAdmin {
-        msg.sender.transfer(amount);
+    function withdraw(uint256 _amount) public onlySuperAdmin {
+        msg.sender.transfer(_amount);
     }
 
     // normal
     /**
+     * @title rent user rent the Place
      * @param _id the Place id
      * @param _secondsToRent the time of rental in seconds
+     * @notice Only Whitelisted can execute it.
      */
     function rent(uint256 _id, uint256 _secondsToRent) external payable onlyWhitelisted {
         require(isPlaceExist(_id), "the Place id doesn't exist");
@@ -168,6 +183,8 @@ contract Renting is WhitelistedRole {
     }
 
     /**
+     * @title upgradePlaceStatus upgrade the status of the Place
+     * @param _id the Place id
      * @dev 流程上是租户到期自动终止合同，不用调用合约。但是合约的一些状态此时需要更新，因此任何人都可以调用。
      * @dev 先简单点，不能预约几天后开始的，比如明天到后天的工位。都是直接从 now 到结束时间
      * @dev 因此不需要检测 start
@@ -189,6 +206,10 @@ contract Renting is WhitelistedRole {
         }
     }
 
+    /**
+     * @title upgradeAllPlaceStatus upgrade the status of all Place.
+     * @notice if gas is too high, use upgradeAllPlaceStatus2
+     */
     function upgradeAllPlaceStatus() public {
         for(uint256 i = 0; i < ids.length; i++) {
             uint256 _id = ids[i];
@@ -197,7 +218,10 @@ contract Renting is WhitelistedRole {
     }
 
     /**
-     * @dev upgrade [_a, _b] status
+     * @title upgradeAllPlaceStatus2 upgrade the status of all Place.
+     * @param _a the begin Place'index that want to be upgraded
+     * @param _b the end Place'index that want to be upgraded
+     * @notice upgrade the Place's status that is in ids[_a, _b]
      */
     function upgradeAllPlaceStatus2(uint256 _a, uint256 _b) public {
         require(_a < _b, "error order");
@@ -211,9 +235,10 @@ contract Renting is WhitelistedRole {
     // view
 
     /**
+     * @title getPrice the total price for rentinng the Place
      * @param _id the Place id
-     * @return price to be payed for renting the device
-     * @dev the total price for rentinng the Place
+     * @param _secondsToRent the duration of rent the Place
+     * @return _price price to be payed for renting the Place
      */
     function getPrice(uint256 _id, uint256 _secondsToRent) public view returns (uint256) {
         require(isPlaceExist(_id), "the Place id doesn't exist");
@@ -222,10 +247,10 @@ contract Renting is WhitelistedRole {
     }
 
     /**
-     * @param _status deviceid
-     * @return amount of states of a device
-     * @dev returns the current amount of states
-     * @dev call upgradeAllPlaceStatus() before call this function, or the amount is not accurate;
+     * @title getStateCount get the amount of Place that is in this Status. returns the current amount of states
+     * @param _status the Status
+     * @return _amount amount of states of a device
+     * @notice make sure  upgradeAllPlaceStatus() is executed before call this function, or the amount is not accurate;
      */
     function getStateCount(Status _status) external view returns (uint256) {
         uint256 _amount = 0;
@@ -239,14 +264,17 @@ contract Renting is WhitelistedRole {
     }
 
     /**
-     * @dev get the total amount of Places
+     * @title getTotalAmount get the total amount of Places
+     * @return the amount of Places
      */
     function getTotalAmount() external view returns (uint256) {
         return ids.length;
     }
 
     /**
-     * @return element of ids;
+     * @title getIdFromIndex get in from ids array
+     * @param _index the index of ids array
+     * @return element of ids, that is Place's id
      */
     function getIdFromIndex(uint256 _index) external view returns (uint256) {
         require(_index < ids.length, "error _index");
@@ -254,9 +282,9 @@ contract Renting is WhitelistedRole {
     }
 
     /**
+     * @title getPlaceInfo get the detail infomation of a Place
      * @param _id Place id
-     * @return controller, rentedFrom, rentedUntil and properties of the given index beloning to the state
-     * @dev returns the important informations of a state
+     * @return status, user, begin and end infomation of the Place.
      */
     function getPlaceInfo(uint256 _id) external view returns (Status _status, address _user, uint256 _begin, uint256 _end, uint256 _perPrice) {
         require(isPlaceExist(_id), "the place _id is not exists");
@@ -268,36 +296,67 @@ contract Renting is WhitelistedRole {
         _perPrice = places[_id].perPrice;
     }
 
+    /**
+     * @title getPlaceState get the State infomation of a Place
+     * @param _id Place id
+     * @return status of the Place.
+     * @notice 0 stand for Stop; 1 stand for Free, 2 stand for OnWork
+     */
     function getPlaceState(uint256 _id) public view returns(Status) {
         require(isPlaceExist(_id), "the place _id is not exists");
 
         return places[_id].status;
     }
 
+    /**
+     * @title getPlaceUser get the user infomation of a Place
+     * @param _id Place id
+     * @return user address of the Place.
+     */
     function getPlaceUser(uint256 _id) public view returns(address) {
         require(isPlaceExist(_id), "the place _id is not exists");
 
         return places[_id].user;
     }
 
+    /**
+     * @title getPlaceBegin get the begin infomation of a Place
+     * @param _id Place id
+     * @return begin's Unix timestamp of the Place.
+     */
     function getPlaceBegin(uint256 _id) public view returns(uint256) {
         require(isPlaceExist(_id), "the place _id is not exists");
 
         return places[_id].begin;
     }
 
+    /**
+     * @title getPlaceEnd get the end infomation of a Place
+     * @param _id Place id
+     * @return end's Unix timestamp of the Place.
+     */
     function getPlaceEnd(uint256 _id) public view returns(uint256) {
         require(isPlaceExist(_id), "the place _id is not exists");
 
         return places[_id].end;
     }
 
+    /**
+     * @title getPlacePerPrice get the perPrice infomation of a Place
+     * @param _id Place id
+     * @return the perPrice of the Place.
+     */
     function getPlacePerPrice(uint256 _id) public view returns(uint256) {
         require(isPlaceExist(_id), "the place _id is not exists");
 
         return places[_id].perPrice;
     }
 
+    /**
+     * @title isPlaceExist judge whether a Place exist
+     * @param _id Place id
+     * @return if exist, true
+     */
     function isPlaceExist(uint256 _id) public view returns (bool) {
         if(places[_id].id != 0) {
             return true;
